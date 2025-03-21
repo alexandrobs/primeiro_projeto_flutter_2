59f2a34bbbf732d852e5120180f00d9b68927cb4
Alexandro B S <jornalsinal@gmail.com>
2025-03-21 19:35:25 -0300

    Estudos de Dart 2025 Variáveis e Tipos de Variáveis

---
+++ lib/estudos.dart
@@ -0,0 +1,26 @@
+//Estudos em Dart 2025
+
+/*
+Comentários
+de mais de uma
+linha
+ */
+
+void main() {
+  for (int i = 0; i < 5; i++) {
+    print('hello ${i+ 1}');
+  }
+
+  var nome;
+  nome = "João";
+  print(nome);
+
+  const pi = 3.14;
+  print(pi);
+
+  var salario = 100;
+  var bonus = 20;
+  var total = salario + bonus;
+  print("O salário é: $total");
+
+}
