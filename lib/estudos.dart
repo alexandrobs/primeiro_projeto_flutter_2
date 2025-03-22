fc8b6d232e9d3477c6354998d24a002bbf443804
Alexandro B S <jornalsinal@gmail.com>
2025-03-22 19:09:54 -0300

    Estudos de Dart 2025 Arrays e Operadores aritméticos

--- lib/estudos.dart
+++ lib/estudos.dart
@@ -7,6 +7,9 @@ linha
  */
 
 void main() {
+
+  //Variáveis com tipo implicito e com tipo explicito
+
   for (int i = 0; i < 5; i++) {
     print('hello ${i+ 1}');
   }
@@ -29,5 +32,37 @@ void main() {
   bool acesso = true;
   
   print("Email: $email, número: $numero, preço: $preco, acesso: $acesso");
+
+  //Arrays
+  var nomes = ["Jamilton","Alex","Sofia", "Maria", 100];
+  var numeros = [02,15,36, 98, 100];
+  print(nomes[0]);
+  print(nomes[1]);
+  print(nomes[2]);
+  print(nomes[3]);
+  print(nomes[4]);
+
+  print(numeros[0]);
+  print(numeros[1]);
+  print(numeros[2]);
+  print(numeros[3]);
+  print(numeros[4]);
+
+  //Operadores aritmeticos
+  //+, -, /, *
+  var resultado = 1 + 1;
+  print( resultado);
+  //operador resumido
+  var numero2 = 10;
+  numero2 += 15;
+  print(numero2);
+  numero2 -= 9;
+  print(numero2);
+  numero2 *= 10;
+  print(numero);
+  numero2++;
+  print(numero2);
+  numero2--;
+  print(numero2);
 
 }
